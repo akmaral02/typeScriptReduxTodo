ce2a02b3c6b77f3e5fd3b48ac90fa96a13172f7a
akmaral02 <akmaral.abdylmanap@gmail.com>
2023-05-04 10:28:19 +0600

    ..

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -13,38 +13,40 @@ const TodoItem: React.FC<TodoItemProps> = ({ id, title, completed }) => {
   const dispatch = useAppDispatch();
 
   return (
-    <Box
-      sx={{
-        width: "33%",
-        height: 50,
-        // backgroundColor: "gray",
-        // opacity: 0.5,
-        backgroundColor: "#DCDCDC",
-        borderRadius: 3,
-        display: "flex",
-        justifyContent: "space-around",
-        alignItems: "center",
-      }}
-    >
-      <input
-        type="checkbox"
-        checked={completed}
-        onChange={() => dispatch(toggleComplete(id))}
-      />
-      <span>{title}</span>
-      {/* <Box
+    <Box width={"50%"} margin={"auto"}>
+      <Box
+        sx={{
+          width: "100%",
+          height: 50,
+          // backgroundColor: "gray",
+          // opacity: 0.5,
+          backgroundColor: "#DCDCDC",
+          borderRadius: 3,
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+      >
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={() => dispatch(toggleComplete(id))}
+        />
+        <span>{title}</span>
+        {/* <Box
         sx={{
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
         }}
-      >
+        >
         <Typography color={"black"}>{title}</Typography>
       </Box> */}
-      {/* <span onClick={() => dispatch(removeTodo(id))}>&times;</span> */}
-      <Button onClick={() => dispatch(removeTodo(id))}>
-        <DeleteIcon sx={{ color: "red" }}></DeleteIcon>
-      </Button>
+        {/* <span onClick={() => dispatch(removeTodo(id))}>&times;</span> */}
+        <Button onClick={() => dispatch(removeTodo(id))}>
+          <DeleteIcon sx={{ color: "red" }}></DeleteIcon>
+        </Button>
+      </Box>
     </Box>
   );
 };
